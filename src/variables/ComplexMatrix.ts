b8d91d2c9d452afffd9e6778f1a1dc433f377f47
Paulo Silva <paulo.fernando.silva@gmail.com>
2018-11-07 21:09:16 +0900

    Fixed a couple of @param comments.

--- src/variables/ComplexMatrix.ts
+++ src/variables/ComplexMatrix.ts
@@ -10,6 +10,7 @@ export class ComplexMatrix extends Matrix {
 	 * @param freeIndices the number of elements in each dimension.
 	 * @param fixedIndices if this is a part of a larger matrix, the right-most
 	 * indices that are used to access this submatrix (one based).
+	 * @param validValue actually the variable content and not a placeholder?
 	 **************************************************************************/
 	constructor(
 		name: string = '',
@@ -19,7 +20,7 @@ export class ComplexMatrix extends Matrix {
 		validValue: boolean = true
 	)
 	{
-		super(name, value, freeIndices, fixedIndices);
+		super(name, value, freeIndices, fixedIndices, validValue);
 	}
 
 
--- src/variables/Matrix.ts
+++ src/variables/Matrix.ts
@@ -27,6 +27,7 @@ export class Matrix extends Variable {
 	 * @param freeIndices the number of elements in each dimension.
 	 * @param fixedIndices if this is a part of a larger matrix, the right-most
 	 * indices that are used to access this submatrix (one based).
+	 * @param validValue actually the variable content and not a placeholder?
 	 **************************************************************************/
 	constructor(
 		name: string = '',
