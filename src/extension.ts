5d74e1e63d9433c1cfb9d29a1437d7f0a1b5f3fc
paulo <paulo.fernando.silva@gmail.com>
2019-12-18 11:23:38 +0100

    Tidy invalid config value checking.

--- src/extension.ts
+++ src/extension.ts
@@ -38,6 +38,10 @@ export function deactivate() {
 
 //******************************************************************************
 class OctaveDebuggerConfigurationProvider implements vscode.DebugConfigurationProvider {
+	undef(value: any) {
+		return value === undefined || value === null;
+	}
+
 	/**
 	 * Massage a debug configuration just before a debug session is being launched,
 	 * e.g. add all missing attributes to the debug configuration.
@@ -50,12 +54,13 @@ class OctaveDebuggerConfigurationProvider implements vscode.DebugConfigurationPr
 	{
 		const editor = vscode.window.activeTextEditor;
 		if(editor && editor.document.languageId === Constants.LANGUAGE) {
-			if(!config.type)			{ config.type = Constants.DEBUGGER_TYPE; }
-			if(!config.name)			{ config.name = 'Debug ${file}'; }
-			if(!config.request) 		{ config.request = 'launch'; }
-			if(!config.program) 		{ config.program = '${file}'; }
-			if(!config.octave)			{ config.octave = Constants.DEFAULT_EXECUTABLE; }
-			if(!config.sourceFolder)	{ config.sourceFolder = '${workspaceFolder}'; }
+			if(this.undef(config.type))				{ config.type = Constants.DEBUGGER_TYPE; }
+			if(this.undef(config.name))				{ config.name = 'Debug ${file}'; }
+			if(this.undef(config.request)) 			{ config.request = 'launch'; }
+			if(this.undef(config.program)) 			{ config.program = '${file}'; }
+			if(this.undef(config.octave))			{ config.octave = Constants.DEFAULT_EXECUTABLE; }
+			if(this.undef(config.sourceFolder))		{ config.sourceFolder = '${workspaceFolder}'; }
+			if(this.undef(config.autoTerminate))	{ config.autoTerminate = true; }
 		}
 
 		return config;
