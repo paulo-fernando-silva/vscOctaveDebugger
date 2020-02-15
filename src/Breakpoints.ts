4a755093674114a8823dd43692ba799ad0205bdd
paulo <paulo.fernando.silva@gmail.com>
2020-02-15 19:19:51 +0900

    Added confirmation for both unconditional and conditional breakpoints.

--- src/Breakpoints.ts
+++ src/Breakpoints.ts
@@ -9,11 +9,7 @@ type ConditionalBreakpoint = DebugProtocol.SourceBreakpoint;
 
 //******************************************************************************
 export class Breakpoints {
-	private static _breakpointId: number = 0;
-
-
 	//**************************************************************************
-	private static readonly BP_REGEX = /^\s*(?:ans =)?\s*((?:\d+\s*)+)$/;
 	public static set(
 		breakpoints: Array<ConditionalBreakpoint>,
 		path: string,
@@ -21,34 +17,109 @@ export class Breakpoints {
 		callback: (breakpoints: Array<Breakpoint>) => void
 	): void
 	{
-		const confirmedBreakpoints = new Array<Breakpoint>();
+		const confirmed = new Array<Breakpoint>();
+
 		if(breakpoints.length !== 0) {
-			const fname = functionFromPath(path);
-			let lines = '';
+			const conditional = new Array<ConditionalBreakpoint>();
+			const unconditional = new Array<ConditionalBreakpoint>();
+			// Split breakpoints between conditional and unconditional
 			breakpoints.forEach(b => {
+				b.column = confirmed.length;
+				// Breakpoints start unconfirmed, and the column is their index
+				confirmed.push(new Breakpoint(false, b.line));
+
 				if(b.condition !== undefined && b.condition.length !== 0) {
-					// TODO: shouldn't these also be confirmed?
-					runtime.execute(`dbstop in ${fname} at ${b.line} if ${b.condition}`);
+					conditional.push(b);
 				} else {
-					lines += `${b.line} `;
+					unconditional.push(b);
+				}
+			});
+			// Actually set the breakpoints
+			const fname = functionFromPath(path);
+			if(unconditional.length !== 0) { // any unconditional breakpoints?
+				if(conditional.length !== 0) { // both breakpoint types?
+					Breakpoints.setUnconditional(unconditional, confirmed, fname, runtime,
+						(confirmed: Array<Breakpoint>) => {
+							Breakpoints.setConditional(conditional, confirmed, fname, runtime, callback);
+						});
+				} else { // only unconditional breakpoints
+					Breakpoints.setUnconditional(unconditional, confirmed, fname, runtime, callback);
+				}
+			} else { // only conditional breakpoints
+				Breakpoints.setConditional(conditional, confirmed, fname, runtime, callback);
+			}
+		} else {
+			callback(confirmed);
+		}
+	}
+
+
+	//**************************************************************************
+	private static readonly BP_REGEX = /^\s*(?:ans =)?\s*((?:\d+\s*)+)$/;
+	private static setUnconditional(
+		breakpoints: Array<ConditionalBreakpoint>,
+		confirmedBreakpoints: Array<Breakpoint>,
+		fname: string,
+		runtime: CommandInterface,
+		callback: (breakpoints: Array<Breakpoint>) => void
+	): void
+	{
+		let lines = '';
+		breakpoints.forEach(b => lines += `${b.line} `);
+		let i = 0;
+
+		runtime.evaluate(`dbstop ${fname} ${lines}`, (output: string[]) => {
+			output.forEach(line => {
+				const match = line.match(Breakpoints.BP_REGEX);
+
+				if(match !== null && match.length === 2) {
+					const lines = match[1].split(' ').filter((val) => val);
+					lines.forEach(l => {
+						const bp = breakpoints[i++];
+						const idx = (bp.column? bp.column : 0);
+						confirmedBreakpoints[idx] = new Breakpoint(true, parseInt(l));
+					});
 				}
 			});
 
-			runtime.evaluate(`dbstop ${fname} ${lines}`, (output: string[]) => {
+			if(i === breakpoints.length) {
+				callback(confirmedBreakpoints);
+			}
+		});
+	}
+
+
+	//**************************************************************************
+	private static setConditional(
+		breakpoints: Array<ConditionalBreakpoint>,
+		confirmedBreakpoints: Array<Breakpoint>,
+		fname: string,
+		runtime: CommandInterface,
+		callback: (breakpoints: Array<Breakpoint>) => void
+	): void
+	{
+		let i  = 0;
+
+		breakpoints.forEach(b => {
+			const expression = `dbstop in ${fname} at ${b.line} if ${b.condition}`;
+
+			runtime.evaluate(expression, (output: string[]) => {
 				output.forEach(line => {
 					const match = line.match(Breakpoints.BP_REGEX);
 
 					if(match !== null && match.length === 2) {
-						const lines = match[1].split(' ').filter((val) => val);
-						lines.forEach(l => confirmedBreakpoints.push(this.toBreakpoint(l)));
+						const l = match[1];
+						const bp = breakpoints[i++];
+						const idx = (bp.column? bp.column : 0);
+						confirmedBreakpoints[idx] = new Breakpoint(true, parseInt(l));
 					}
 				});
 
-				callback(confirmedBreakpoints);
+				if(i === breakpoints.length) {
+					callback(confirmedBreakpoints);
+				}
 			});
-		} else {
-			callback(confirmedBreakpoints);
-		}
+		});
 	}
 
 
@@ -75,16 +146,6 @@ export class Breakpoints {
 	{
 		const func = functionFromPath(path);
 		runtime.execute(`dbclear ${func} ${lines}`);
-	}
-
-
-	//**************************************************************************
-	private static toBreakpoint(lineNumber: string): Breakpoint {
-		return <Breakpoint>{
-			verified: true,
-			id: this._breakpointId++,
-			line: parseInt(lineNumber)
-		};
 	}
 
 
